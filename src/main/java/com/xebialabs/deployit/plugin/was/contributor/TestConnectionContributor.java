920132853e358ca402c1e66fff38c582b334d719
Benoit Moussaud <bmoussaud@xebialabs.com>
2011-09-20 10:59:11 +0200

    Guava style

--- src/main/java/com/xebialabs/deployit/plugin/was/contributor/TestConnectionContributor.java
+++ src/main/java/com/xebialabs/deployit/plugin/was/contributor/TestConnectionContributor.java
@@ -2,6 +2,7 @@ package com.xebialabs.deployit.plugin.was.contributor;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 import com.xebialabs.deployit.plugin.api.deployment.execution.DeploymentStep;
@@ -22,6 +23,7 @@ import java.util.Set;
 
 import static com.google.common.collect.Collections2.filter;
 import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.ImmutableMap.of;
 import static com.google.common.collect.Maps.newHashMap;
 
 /**
@@ -43,14 +45,8 @@ public class TestConnectionContributor {
                     deployed.<Integer>getSyntheticProperty("testConnectionOrder"),
                     deployed.getContainer().getManagingContainer(),
                     deployed.<String>getSyntheticProperty("testConnectionScript"),
-                    getPythonVars(deployed),
+                    of("deployed", (Object) deployed),
                     "Test Connection " + deployed.getName());
-        }
-
-        private Map<String, Object> getPythonVars(Deployed deployed) {
-            Map<String, Object> pythonVars = newHashMap();
-            pythonVars.put("deployed", deployed);
-            return pythonVars;
         }
     };
 
